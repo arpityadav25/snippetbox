71b451b95c8dfcbef20f91d9790e2292b80a4479
arpityadav25 <arpityadav2514613@gmail.com>
2025-05-03 22:10:44 +0530

    added user authentication, logging in and out

--- internal/models/user.go
+++ internal/models/user.go
@@ -36,8 +36,7 @@ func (m *UserModel) Insert(name, email, password string) error {
 	if err != nil {
 		var mySQLError *mysql.MySQLError
 		if errors.As(err, &mySQLError) {
-			if mySQLError.Number == 1062 &&
-				strings.Contains(mySQLError.Message, "users_uc_email") {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users.user_uc_email") {
 				return ErrDuplicateEmail
 			}
 		}
@@ -47,7 +46,31 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
+
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
